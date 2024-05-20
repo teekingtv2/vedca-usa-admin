f7a7a6d1f68ef8261260457bbf576643157b8b92
borderlesstravels <borderlesstravels.africa@gmail.com>
2024-05-20 23:53:33 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AdPage from './pages/AdPage';
 import ImagesFooter from './components/ImagesFooter';
 import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,7 +13,8 @@ function App() {
       <BrowserRouter>
         <div className="main">
           <Routes>
-            <Route path="/:adSlug" element={<AdPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/add-1" element={<AdPage />} />
           </Routes>
         </div>
         <ImagesFooter />
---
+++ src/components/HomeBody.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomeBody = () => {
+  return (
+    <div>
+      <h1>Welcome here</h1>
+    </div>
+  );
+};
+
+export default HomeBody;
