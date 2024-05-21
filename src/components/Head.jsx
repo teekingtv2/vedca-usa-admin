395ba80c10a2bbab4bd0516ed3a513240bff3aee
borderlesstravels <borderlesstravels.africa@gmail.com>
2024-05-21 13:25:37 +0100

    latest commit

---
+++ src/components/Head.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Head = ({ pageTitle }) => {
+  return (
+    <Helmet>
+      <title>Hedge Funds - {pageTitle}</title>
+      <meta
+        name="description"
+        content={`$Hedge Funds is a cryptocurrency investment trading platform that provides high Returns on Invesments for investors.`}
+      />
+      <meta
+        name="keywords"
+        content="Crypto investments, Cryptocurrency trading groups, trading groups, premium trading groups"
+      />
+      <meta
+        name="author"
+        content="Mudashir Tunde (Tee King) - Jaflah Software Development Company"
+      />
+    </Helmet>
+  );
+};
+
+export default Head;
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -3,21 +3,24 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div className="h-[100%] bg-[#030811] py-[60px] md:py-[100px] px-7 w-[100vw] text-center">
-      <p>
-        The page you are trying to visit does not exist on this app. Looks like you got here by a
-        mistake.
-      </p>
+    <>
+      <Head pageTitle="Page Not Found" />
+      <div className="h-[100%] bg-[#030811] py-[60px] md:py-[100px] px-7 w-[100vw] text-center">
+        <p>
+          The page you are trying to visit does not exist on this app. Looks like you got here by a
+          mistake.
+        </p>
 
-      <Link
-        to="/"
-        style={{
-          fontWeight: '700',
-        }}
-      >
-        Go back to the homepage
-      </Link>
-    </div>
+        <Link
+          to="/"
+          style={{
+            fontWeight: '700',
+          }}
+        >
+          Go back to the homepage
+        </Link>
+      </div>
+    </>
   );
 };
 
