c87d710ac13969fd7ce4d4204d6eb4212e1d7257
borderlesstravels <borderlesstravels.africa@gmail.com>
2024-07-13 17:48:47 +0100

    admin ready 3

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://admin.bitblockservice.com/',
+  base: 'https://vedca-usa-admin.vercel.app/',
   plugins: [react()],
   define: {
     global: 'window',
